d7782ff8e02c564a0043a1778b78b955b4c13ca4
Alexander <maina206mwangi@gmail.com>
2024-10-06 22:30:07 +0300

    DOM modification code grade complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const mainId = document.getElementById("main");
+mainId.remove();
+
+//create the h1 element
+const newHeader = document.createElement("h1");
+//set the id attribute to be victory
+newHeader.setAttribute("id", "victory");
+//set the h1 content to be Alex is the champion
+newHeader.textContent = "Alex is the champion";
+//add the element together wth the properties to the DOM
+document.body.append(newHeader);
+console.log(newHeader.id);
